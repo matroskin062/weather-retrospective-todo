ca62d6c97d54dab2cbb652fb48d3900e4078aabd
Dima Matroskin <dima.matroskin062@gmail.com>
2021-03-06 16:30:52 +0200

    BrowserRouter moved to index file and route for no match added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
 import Retro from './components/Retro';
 import TodoList from './components/Todo';
 import Weather from './components/Weather';
@@ -7,18 +7,19 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='App'>
-        <div className='Navigation'>
-          <Link to='/todo'>Todo</Link>
-          <Link to='/retro'>Retro</Link>
-          <Link to='/weather'>Weather</Link>
-        </div>
+    <div className='App'>
+      <div className='Navigation'>
+        <Link to='/todo'>Todo</Link>
+        <Link to='/retro'>Retro</Link>
+        <Link to='/weather'>Weather</Link>
+      </div>
+      <Switch>
         <Route path='/weather' component={Weather} />
         <Route path='/retro' component={Retro} />
         <Route path='/todo' component={TodoList} />
-      </div>
-    </BrowserRouter>
+        <Route path='*' render={() => <Redirect to='/weather' />} />
+      </Switch>
+    </div>
   );
 }
 
